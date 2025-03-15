79a8aceb96590d96c7684e2d99b10aae976508e2
Amit Kumar <amitk200415@gmail.com>
2025-03-15 21:06:51 +0530

    ADD: send verification email

---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,27 @@
+import { resend } from "@/lib/resend";
+import VerificationEmail from "../../emails/verificationEmail";
+import { apiResponse } from "../../types/apiResponse";
+
+export async function sendVerificationEmail(
+    username: string,
+    email: string,
+    verifyCode: string,
+): Promise<apiResponse>{
+    try {
+        await resend.emails.send({
+            from: '<onboarding@resend.dev>',
+            to: email,
+            subject: 'Mystery Message verification code',
+            react: VerificationEmail({username, otp: verifyCode}),
+        });
+
+        console.log("verification code send successfully");
+
+        return {success: true, message: "Verification email sent successfully"}
+    } catch (emailError) {
+        console.log("error sending verification code");
+
+        return {success: false, message: "Failed to send verification email."}
+    }
+}
+
